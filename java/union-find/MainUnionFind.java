3e2ea4e3992e2dd6dfa1ab4e8aaeab6265e66d3a
Erick <ahmederick00@gmail.com>
2022-06-15 19:15:56 +0200

    Implementation of Union-Find algorithm

---
+++ java/union-find/MainUnionFind.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MainUnionFind {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        UnionFind uf = new UnionFind(n);
+
+        int p = sc.nextInt();
+        int q = sc.nextInt();
+
+        if (!uf.find(p, q)) {
+            uf.union(p, q);
+            System.out.println(p + " " + q);
+        }
+    }
+}
